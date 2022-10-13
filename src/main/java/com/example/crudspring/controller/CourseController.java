dafe3bb2bc349c550e86238ddafc61fcbb8da1f7
Ramon Cruz <ramon.rodrigo2020@hotmail.com>
2022-10-13 20:25:52 -0300

    API Spring: Buscar por ID

--- src/main/java/com/example/crudspring/controller/CourseController.java
+++ src/main/java/com/example/crudspring/controller/CourseController.java
@@ -1,12 +1,17 @@
 package com.example.crudspring.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +30,12 @@ public class CourseController {
     @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{courseId}")
+    public ResponseEntity<Course> findById(@PathVariable Long courseId){
+        return courseRepository.findById(courseId).map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
