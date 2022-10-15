bf68a4e71e47662080d31eb296e8cad9fedf0ad0
Ramon Cruz <ramon.rodrigo2020@hotmail.com>
2022-10-14 21:30:51 -0300

    Test HTTP PUT

--- src/main/java/com/example/crudspring/controller/CourseController.java
+++ src/main/java/com/example/crudspring/controller/CourseController.java
@@ -1,17 +1,14 @@
 package com.example.crudspring.controller;
 
 import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +39,15 @@ public class CourseController {
     @ResponseStatus(HttpStatus.CREATED)
     public Course create(@RequestBody Course course){
         return this.courseRepository.save(course);
+    }
+
+    @PutMapping("/{courseId}")
+    public ResponseEntity<Course> update(@PathVariable Long courseId, @RequestBody Course course){
+        if(!courseRepository.existsById(courseId)){
+            return ResponseEntity.notFound().build();
+        }
+        courseRepository.save(course);
+        return ResponseEntity.ok(course);
     }
 
 }
